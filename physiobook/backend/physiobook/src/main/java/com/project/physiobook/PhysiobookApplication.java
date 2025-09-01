0894c5a4d4b50455212a81c37d854dc51689d8dd
Abhishek sukhadev wankhade <119746259+abhishek853@users.noreply.github.com>
2025-09-01 11:59:33 +0530

    Add files via upload

---
+++ physiobook/backend/physiobook/src/main/java/com/project/physiobook/PhysiobookApplication.java
@@ -0,0 +1,13 @@
+package com.project.physiobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysiobookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysiobookApplication.class, args);
+	}
+
+}
---
+++ physiobook/backend/physiobook/src/test/java/com/project/physiobook/PhysiobookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.physiobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysiobookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
