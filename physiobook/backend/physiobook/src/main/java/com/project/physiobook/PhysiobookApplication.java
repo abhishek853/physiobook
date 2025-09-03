89bd144c41c49dbca6446780c30d3058e33ec5ea
harishchopde <harishchopade2004@gmail.com>
2025-09-03 17:43:52 +0530

    Change in main file

--- physiobook/backend/physiobook/src/main/java/com/project/physiobook/PhysiobookApplication.java
+++ physiobook/backend/physiobook/src/main/java/com/project/physiobook/PhysiobookApplication.java
@@ -8,7 +8,7 @@ public class PhysiobookApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhysiobookApplication.class, args);
-		System.out.println("hello");
+		System.out.println("Application Running ");
 	}
 
 }
